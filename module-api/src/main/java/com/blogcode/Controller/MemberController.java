bb2808e5a62f3a242657b06acd171416aaff2d0c
alsyean <47862141+alsyean@users.noreply.github.com>
2020-11-23 11:09:48 +0900

    Update MemberController.java

--- module-api/src/main/java/com/blogcode/Controller/MemberController.java
+++ module-api/src/main/java/com/blogcode/Controller/MemberController.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @NoArgsConstructor
 @RestController

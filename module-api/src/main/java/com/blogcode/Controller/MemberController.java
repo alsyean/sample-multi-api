bed026ffc250e1f9340c4a72727cd9558e7fafe2
alsyean <47862141+alsyean@users.noreply.github.com>
2020-11-23 11:10:18 +0900

    Update MemberController.java

--- module-api/src/main/java/com/blogcode/Controller/MemberController.java
+++ module-api/src/main/java/com/blogcode/Controller/MemberController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @NoArgsConstructor
 @RestController
